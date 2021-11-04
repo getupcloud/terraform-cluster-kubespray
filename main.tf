aeb86a907d73e962ecb64a06b73683907097b441
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2021-11-04 13:14:52 -0300

    wip

--- main.tf
+++ main.tf
@@ -2,10 +2,23 @@ module "internet" {
   source = "github.com/getupcloud/terraform-module-internet?ref=main"
 }
 
-module "flux" {
-  source = "github.com/getupcloud/terraform-module-flux?ref=main"
+#module "flux" {
+#  source = "github.com/getupcloud/terraform-module-flux?ref=main"
+#
+#  git_repo       = var.flux_git_repo
+#  manifests_path = "./clusters/${var.name}/kubespray/manifests"
+#  wait           = var.flux_wait
+#}
 
-  git_repo       = var.flux_git_repo
-  manifests_path = "./clusters/${var.name}/eks/manifests"
-  wait           = var.flux_wait
+resource "shell_script" "kubespray-repo" {
+  lifecycle_commands {
+    create = local.git_checkout
+    update = local.git_checkout
+    read   = local.git_state
+    delete = local.git_reset
+  }
+
+  environment = {
+    KUBESPRAY_GIT_REF = var.kubespray_git_ref
+  }
 }
