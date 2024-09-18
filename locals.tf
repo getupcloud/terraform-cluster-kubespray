3dee06cfc4ef726b856c57fe02c9dd7bc0f1bb45
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-09-18 14:33:48 +0100

    Fix node labels/taints

--- locals.tf
+++ locals.tf
@@ -1,29 +1,32 @@
 locals {
   kubeconfig_filename = abspath(pathexpand(var.kubeconfig_filename))
 
-  master_nodes = [for i, node in var.master_nodes : merge({
-    node_type : "master"
-    hostname : "master-${i}"
-    disks : {}
-    labels : var.default_master_node_labels
-    taints : var.default_master_node_taints
-  }, node)]
+  master_nodes = [for i, node in var.master_nodes : {
+    node_type : try(node.node_type, "master")
+    hostname : try(node.hostname, "")
+    address : try(node.address, "")
+    disks : try(node.disks, {})
+    node_labels : merge(var.default_master_node_labels, try(node.node_labels, {}))
+    node_taints : toset(concat(var.default_master_node_taints, try(node.node_taints, [])))
+  }]
 
-  infra_nodes = [for i, node in var.infra_nodes : merge({
-    node_type : "worker"
-    hostname : "infra-${i}"
-    disks : {}
-    labels : var.default_infra_node_labels
-    taints : var.default_infra_node_taints
-  }, node)]
+  infra_nodes = [for i, node in var.infra_nodes : {
+    node_type : try(node.node_type, "infra")
+    hostname : try(node.hostname, "")
+    address : try(node.address, "")
+    disks : try(node.disks, {})
+    node_labels : merge(var.default_infra_node_labels, try(node.node_labels, {}))
+    node_taints : toset(concat(var.default_infra_node_taints, try(node.node_taints, [])))
+  }]
 
-  app_nodes = [for i, node in var.app_nodes : merge({
-    node_type : "worker"
-    hostname : "app-${i}"
-    disks : {}
-    labels : var.default_app_node_labels
-    taints : var.default_app_node_taints
-  }, node)]
+  app_nodes = [for i, node in var.app_nodes : {
+    node_type : try(node.node_type, "worker")
+    hostname : try(node.hostname, "")
+    address : try(node.address, "")
+    disks : try(node.disks, {})
+    node_labels : merge(var.default_app_node_labels, try(node.node_labels, {}))
+    node_taints : toset(concat(var.default_app_node_taints, try(node.node_taints, [])))
+  }]
 
   all_nodes = concat(local.master_nodes, local.infra_nodes, local.app_nodes)
 
